80ed3572c98910bd3e40b06099a3f97033e3ffea
upmathematics <quijano@up.edu>
2026-02-05 09:41:02 -0800

    topics update

---
+++ docs/lesson/3.2-mth361-random-variables-and-probability/cumulative-fair-coin-flips-sim.R
@@ -0,0 +1,49 @@
+# load packages
+library(tidyverse)
+library(openintro)
+library(gganimate)
+library(latex2exp)
+data(COL)
+seed <- 111
+
+# set hyperparameters
+set.seed(seed) # for reproducibility
+n <- seq(1,1000) # number of trials as a vector
+
+# set parameter and PMF of the Bernoulli r.v
+X <- c(0,1) # outcomes ("0"="failure","1"="success")
+p <- 0.50 # probability of success
+bern_pmf <- c(1-p,p)
+
+# simulate Bernoulli trials while tracking n and the proportions
+samples <- sample(X,size=max(n),prob=bern_pmf,replace=TRUE)
+props_1 <- c()
+for (i in n){
+  props_1 <- c(props_1,sum(samples[1:i])/i)
+}
+df <- tibble(n=n,`H`=props_1) %>%
+  pivot_longer(-n, names_to = "X",values_to = "prop")
+
+# plot simulations
+p3 <- ggplot(df,aes(x=n,y=prop,color=X,group=X)) + 
+  geom_point(size=2) + 
+  geom_line(linewidth=1) + 
+  scale_color_manual(values=c(`H`="#009159")) + 
+  geom_hline(aes(yintercept = p), color = "#f1cc54", linetype="dashed", linewidth=1) + 
+  geom_text(aes(0,p),label = paste(TeX("P(H)")," = ",p,sep=""), 
+            vjust = -1, hjust=-1.75,color="#f1cc54") + 
+  ylim(0,1) + 
+  xlab("n") + 
+  ylab("proportion") + 
+  ggtitle("Simulation of Cumulative Fair Coin Flips") + 
+  theme_minimal() + 
+  theme(legend.title = element_blank())
+
+# animation options
+p3_anim <- p3 + 
+  transition_reveal(n)
+
+# animate and save as .gif
+animate(p3_anim, nframes = round(length(n)/5), 
+        renderer = gifski_renderer("cumulative-fair-coin-flips-sim.gif"),
+        height=1000,width=2000,res=300)
